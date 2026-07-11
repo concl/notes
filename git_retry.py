40c696bbd551cc80ee7defaaf1e88e2579e5d7cf
concl <bonumconcl@gmail.com>
2026-07-11 23:49:55 +0800

    .

--- git_retry.py
+++ git_retry.py
@@ -3,16 +3,24 @@ import re
 from pathlib import Path
 import subprocess
 
+from argparse import ArgumentParser
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--push_or_pull", type=str, default="pull")
+    return parser.parse_args()
 
 def main():
-    
+    args = parse_args()
+    command = args.push_or_pull
     
     path = Path(__file__).parent
     patt = re.compile(r"fatal:.+")
     
     while True:
         
-        process = subprocess.run(["git","pull"],capture_output=True)
+        process = subprocess.run(["git",command],capture_output=True)
         stdout, stderr = process.stdout, process.stderr
         if not patt.match(stderr.decode()):
             break
